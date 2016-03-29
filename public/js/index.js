e5b19a74afd83be4e8cbcfed9b0716ee5657db36
Jason <jasonlikenfs@gmail.com>
2016-03-29 17:52:19 +0800

    add bower stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,11 @@
 var $ = require('jquery');
-var common = require('./common');
+var angular = require('angular');
+var swiper = require('swiper');
+var common = require('./utils');
 
 
 console.log('working...');
 console.log($('h3'));
 console.log(common.showTime());
+console.log(angular.version);
+console.log('swiper: ', swiper);
