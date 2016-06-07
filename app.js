b0dc5d8ae5d2129211f7441f98be9bda8b53ac56
Jason <jasonlikenfs@gmail.com>
2016-06-07 11:33:27 +0800

    remove test router

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-app.get('/test1', function(req, res) {
-  console.log(req.query);
-  res.json({result: 'test1'});
-});
-app.get('/test2', function(req, res) {
-  console.log(req.query);
-  res.json({result: 'test2'});
-});
-
-
 myApp.handleCommonError(app);
 
 module.exports = app;
